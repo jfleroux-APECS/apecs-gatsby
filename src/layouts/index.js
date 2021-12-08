185e8b1905a32da037808ca8ea0f7bf36f4a9ed0
edieval <emmanuel.dieval@adeo.com>
2021-12-08 18:27:17 +0100

    feat: add arrow to go up

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,18 +12,23 @@ const Layout = ({ children }) => {
 
         <div className="level-right">
           <p className="level-item">
-            <FontAwesomeIcon icon={faGift} color="white" size="2x" />&nbsp;
-              <span className="is-hidden-touch">Boutique</span>
+            <FontAwesomeIcon icon={faGift} color="white" size="2x" />
+            &nbsp;
+            <span className="is-hidden-touch">Boutique</span>
           </p>
           <p className="level-item mr-2">
-            <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />&nbsp;
-              <span className="is-hidden-touch">Contact</span>
+            <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />
+            &nbsp;
+            <span className="is-hidden-touch">Contact</span>
           </p>
         </div>
       </div>
       <Navbar />
       {children}
       <Footer />
+      <a href="#" className="button is-floating is-small is-rounded">
+        <i className="fas fa-chevron-up"></i>
+      </a>
     </main>
   );
 };
