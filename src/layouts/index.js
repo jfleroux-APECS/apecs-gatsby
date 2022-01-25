94df20a11ffd0997b037a040f9ef9ebfc6811efa
edieval <emmanuel.dieval@adeo.com>
2022-01-25 21:39:35 +0100

    feat: add link to shop and contact

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMobileAlt, faGift } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import { Link } from "gatsby";
 
 const Layout = ({ children }) => {
   return (
@@ -12,14 +13,18 @@ const Layout = ({ children }) => {
 
         <div className="level-right">
           <p className="level-item">
-            <FontAwesomeIcon icon={faGift} color="white" size="2x" />
-            &nbsp;
-            <span className="is-hidden-touch">Boutique</span>
+            <Link className="top-bandeau-link" to="/support/shop/Shop">
+              <FontAwesomeIcon icon={faGift} color="white" size="2x" />
+              &nbsp;
+              <span className="is-hidden-touch">Boutique</span>
+            </Link>
           </p>
           <p className="level-item mr-2">
-            <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />
-            &nbsp;
-            <span className="is-hidden-touch">Contact</span>
+            <Link className="top-bandeau-link" to="/association/Contact">
+              <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />
+              &nbsp;
+              <span className="is-hidden-touch">Contact</span>
+            </Link>
           </p>
         </div>
       </div>
