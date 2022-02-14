2fbc628e94183413ad47f65d21fad5a0f19e24c3
edieval <emmanuel.dieval@adeo.com>
2022-02-14 22:37:36 +0100

    style: fix navbar on top

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,31 +4,37 @@ import { faMobileAlt, faGift } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import { Link } from "gatsby";
+import "./index.scss";
 
 const Layout = ({ children }) => {
   return (
     <main>
-      <div id="top-bandeau" className="level is-mobile mb-0 background-colored is-size-7-mobile">
-        <div className="level-left"></div>
+      <section className="section p-0 is-fixed-top">
+        <div
+          id="top-bandeau"
+          className="level is-mobile mb-0 background-colored is-size-7-mobile is-fixed-top"
+        >
+          <div className="level-left"></div>
 
-        <div className="level-right">
-          <p className="level-item">
-            <Link className="top-bandeau-link" to="/support/shop/Shop">
-              <FontAwesomeIcon icon={faGift} color="white" size="2x" />
-              &nbsp;
-              <span className="is-hidden-touch">Boutique</span>
-            </Link>
-          </p>
-          <p className="level-item mr-2">
-            <Link className="top-bandeau-link" to="/association/Contact">
-              <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />
-              &nbsp;
-              <span className="is-hidden-touch">Contact</span>
-            </Link>
-          </p>
+          <div className="level-right">
+            <p className="level-item">
+              <Link className="top-bandeau-link" to="/support/shop/Shop">
+                <FontAwesomeIcon icon={faGift} color="white" size="2x" />
+                &nbsp;
+                <span className="is-hidden-touch">Boutique</span>
+              </Link>
+            </p>
+            <p className="level-item mr-2">
+              <Link className="top-bandeau-link" to="/association/Contact">
+                <FontAwesomeIcon icon={faMobileAlt} color="white" size="2x" />
+                &nbsp;
+                <span className="is-hidden-touch">Contact</span>
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
-      <Navbar />
+        <Navbar />
+      </section>
       {children}
       <Footer />
       <a href="#top" className="button is-floating is-small is-rounded">
