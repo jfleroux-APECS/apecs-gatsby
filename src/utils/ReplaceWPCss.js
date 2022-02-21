96027ad410987a5df8371c497772c750786c4e5d
edieval <emmanuel.dieval@adeo.com>
2022-02-21 21:30:42 +0100

    fix: replace Wordpress syntax

--- src/utils/ReplaceWPCss.js
+++ src/utils/ReplaceWPCss.js
@@ -1,8 +1,9 @@
 // const regex = /style=".*?"/g;
 
 export default function replaceWPCss(WPContent) {
-  return WPContent.replaceAll("wp-block-columns", "columns").replaceAll(
-    "wp-block-column",
-    "column"
-  );
+  return WPContent.replaceAll("wp-block-columns", "columns")
+    .replaceAll("wp-block-column", "column")
+    .replaceAll("<h3", '<h3 class="title is-3 my-4"')
+    .replaceAll("<p></p>", "<br/>")
+    .replaceAll("<p", "<p class=\"my-2\"");
 }
