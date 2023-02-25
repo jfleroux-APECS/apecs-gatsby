d08ad851501e30e129ceb4f9b9ca40c608a4e038
Emmanuel Dieval <emmanuel.dieval@adeo.com>
2023-02-25 22:36:53 +0100

    style: complete h2 titles

--- src/utils/ReplaceWPCss.js
+++ src/utils/ReplaceWPCss.js
@@ -5,5 +5,6 @@ export default function replaceWPCss(WPContent) {
     .replaceAll("wp-block-column", "column")
     .replaceAll("<h3", '<h3 class="title is-3 my-4"')
     .replaceAll("<p></p>", "<br/>")
-    .replaceAll("<p", "<p class=\"my-2\"");
+    .replaceAll("<p", "<p class=\"my-2\"")
+    .replaceAll("<h2", '<h2 class="title is-2"');
 }
