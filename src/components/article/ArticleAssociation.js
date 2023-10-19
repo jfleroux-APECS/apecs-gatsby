f6546585f89162ddf64ff1da82357c37e101f021
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-19 16:05:27 +0200

    feat: Create ArticleAssociation.js

---
+++ src/components/article/ArticleAssociation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./article.scss";
+import parse from "html-react-parser";
+import SocialMedia from "../social-media/SocialMedia";
+import replaceWPCss from "../../utils/ReplaceWPCss";
+
+export default function ArticleAssociation(props) {
+  return (
+    <div id={props.id} className="container article-container">
+      <article className="notification article-background mt-5">
+        <div className="columns">
+          <div className="column">
+            <h2 className="title">{parse(props.title)}</h2>
+          </div>
+          <div className="column has-text-right">
+            <SocialMedia title={props.title}></SocialMedia>
+          </div>
+        </div>
+        {parse(replaceWPCss(props.content))}
+      </article>
+    </div>
+  );
+}
