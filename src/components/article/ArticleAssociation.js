a094f09ccd2ad7783fff5f45f826aeed6e7cfc94
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-20 15:46:43 +0200

    fix: syntax errors

--- src/components/article/ArticleAssociation.js
+++ src/components/article/ArticleAssociation.js
@@ -7,30 +7,32 @@ export default function ArticleAssociation(props) {
   return (
     <div>
       {props.featuredImage && (
-          <section
-            id="hero"
-            className="hero is-large background-hero"
-            style={{
-              background:
-                "url(" +
-                props.featuredImage.node.localFile.childImageSharp.gatsbyImageData
-                  .images.fallback.src +
-                ") center center no-repeat fixed",
-            }}
-          >
-            <div className="hero-body"></div>
-          </section>
-        )}
-        <div id={props.id} className="container article-container">
-          <article className="notification article-background mt-5">
+        <section
+          id="hero"
+          className="hero is-large background-hero"
+          style={{
+            background:
+              "url(" +
+              props.featuredImage.node.localFile.childImageSharp.gatsbyImageData
+                .images.fallback.src +
+              ") center center no-repeat fixed",
+          }}
+        >
+          <div className="hero-body"></div>
+        </section>
+      )}
+      <div className="container article-container">
+        <article className="notification article-background mt-5">
+          <div className="columns is-mobile has-text-centered mb-6">
             <div className="columns">
-              <div className="columns">
-                <h2 className="titleBannerLeft">{parse(props.title)}</h2>
-              </div>
+              <h2 className="titleBannerLeft">{parse(props.title)}</h2>
             </div>
+          </div>
+          <div className="columns is-desktop">
             {parse(replaceWPCss(props.content))}
-          </article>
-        </div>
+          </div>
+        </article>
+      </div>
     </div>
   );
 }
