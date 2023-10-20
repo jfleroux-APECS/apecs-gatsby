b303cbee686a440aecd6cb6d92689aaa754a7ebc
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-20 15:15:09 +0200

    test: Matching Templace action

--- src/pages/association/Missions.js
+++ src/pages/association/Missions.js
@@ -1,58 +1,39 @@
 import React from "react";
-import "../app.scss";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql } from "gatsby";
 import ArticleAssociation from "../../components/article/ArticleAssociation";
 
-export default function Missions() {
-  const {
-    allWpPost: { edges: posts },
-  } = useStaticQuery(graphql`
-    query MissionsPostQuery {
-      allWpPost(
-        filter: {
-          categories: { nodes: { elemMatch: { slug: { eq: "nos-missions" } } } }
-        }
-      ) {
-        edges {
-          node {
-            id
-            title
-            content
-            featuredImage {
-              node {
-                altText
-                localFile {
-                  childImageSharp {
-                    gatsbyImageData(
-                      quality: 100
-                      placeholder: DOMINANT_COLOR
-                      layout: FULL_WIDTH
-                    )
-                  }
-                }
-              }
+export const pageQuery = graphql`
+  query ActionsPostById($id: String!) {
+    post: wpPost(id: { eq: $id }) {
+      id
+      excerpt
+      content
+      title
+      date(formatString: "MMMM DD, YYYY")
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                quality: 100
+                placeholder: DOMINANT_COLOR
+                layout: FULL_WIDTH
+              )
             }
-            date(formatString: "DD/MM/YYYY")
           }
         }
       }
     }
-  `);
+  }
+`;
 
+export default function Missions({ data: { post } }) {
   return (
-    <div className="container">
-      {/*<h1 className="APECStitle1">Nos missions</h1>
-      <hr className="divider" />*/}
-      {posts.map((mission, index) => (
-        <div key={mission.node.id}>
-          <ArticleAssociation
-            title={mission.node.title}
-            featuredImage={post.featuredImage}
-            content={mission.node.content}
-          ></ArticleAssociation>
-          <div className="divider is-info" />
-        </div>
-      ))}
-    </div>
+    <ArticleAssociation
+      title={post.title}
+      featuredImage={post.featuredImage}
+      content={replaceWPCss(post.content)}
+    ></ArticleAssociation>
   );
 }
