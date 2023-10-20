af92a20425c2e552c549f278d6e014809dd4b896
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-20 14:37:39 +0200

    test: trying to add principal image on the article

--- src/components/article/ArticleAssociation.js
+++ src/components/article/ArticleAssociation.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./articleassociation.scss";
 import parse from "html-react-parser";
-import SocialMedia from "../social-media/SocialMedia";
 import replaceWPCss from "../../utils/ReplaceWPCss";
 
 export default function ArticleAssociation(props) {
   return (
-    <div id={props.id} className="container article-container">
-      <article className="notification article-background mt-5">
-        <div className="columns">
-          <div className="columns">
-            <h2 className="titleBannerLeft">{parse(props.title)}</h2>
-          </div>
+    <div>
+      {props.featuredImage && (
+          <section
+            id="hero"
+            className="hero is-large background-hero"
+            style={{
+              background:
+                "url(" +
+                props.featuredImage.node.localFile.childImageSharp.gatsbyImageData
+                  .images.fallback.src +
+                ") center center no-repeat fixed",
+            }}
+          >
+            <div className="hero-body"></div>
+          </section>
+        )}
+        <div id={props.id} className="container article-container">
+          <article className="notification article-background mt-5">
+            <div className="columns">
+              <div className="columns">
+                <h2 className="titleBannerLeft">{parse(props.title)}</h2>
+              </div>
+            </div>
+            {parse(replaceWPCss(props.content))}
+          </article>
         </div>
-        {parse(replaceWPCss(props.content))}
-      </article>
     </div>
   );
 }
