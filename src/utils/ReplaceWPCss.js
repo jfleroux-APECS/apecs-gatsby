ce1c75b96ba066899c49b8be92712ac2e239a40d
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-20 11:40:35 +0200

    fix: syntax error

--- src/utils/ReplaceWPCss.js
+++ src/utils/ReplaceWPCss.js
@@ -3,7 +3,6 @@ import "../global.scss";
 export default function replaceWPCss(WPContent) {
   return WPContent.replaceAll("wp-block-columns", "columns")
     .replaceAll("wp-block-column", "column")
-  {/*.replaceAll("<p></p>", "<br/>")*/}
     .replaceAll("<p", '<p class="pApecs my-1"')
     .replaceAll("<h3", '<h3 class="titleApecs2 my-1"')
     .replaceAll("<h2", '<h2 class="titleApecs2 my-3"')
