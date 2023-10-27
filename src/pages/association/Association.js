ceaad381ede342ffbabd8dd7740fbbbf92b7e965
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-27 12:49:12 +0000

    feat: new association page design

--- src/pages/association/Association.js
+++ src/pages/association/Association.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import Article from "../../components/article/Article";
-//test
+import replaceWPCss from "../../utils/ReplaceWPCss";
+import "../app.scss"
+
 
 export default function Association() {
   const {
-    allWpPost: { edges: posts },
+    allWpPost: { edges: postsAsso },
   } = useStaticQuery(graphql`
     query AssociationPostQuery {
       allWpPost(
@@ -39,6 +40,81 @@ export default function Association() {
     }
   `);
 
+
+export default function Missions() {
+  const {
+    allWpPost: { edges: postsMiss },
+  } = useStaticQuery(graphql`
+    query MissionsPostQuery {
+      allWpPost(
+        filter: {
+          categories: { nodes: { elemMatch: { slug: { eq: "nos-missions" } } } }
+        }
+      ) {
+        edges {
+          node {
+            id
+            title
+            content
+            featuredImage {
+              node {
+                altText
+                 localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      quality: 100
+                      placeholder: DOMINANT_COLOR
+                      layout: FULL_WIDTH
+                    )
+                  }
+                }
+              }
+            }
+            date(formatString: "DD/MM/YYYY")
+          }
+        }
+      }
+    }
+  `);
+
+
+export default function NosEquipes() {
+  const {
+    allWpPost: { edges: postsTeam },
+  } = useStaticQuery(graphql`
+    query NosEquipesPostQuery {
+      allWpPost(
+        filter: {
+          categories: { nodes: { elemMatch: { slug: { eq: "equipes" } } } }
+        }
+      ) {
+        edges {
+          node {
+            id
+            title
+            content
+            featuredImage {
+              node {
+                altText
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      quality: 100
+                      placeholder: DOMINANT_COLOR
+                      layout: FULL_WIDTH
+                    )
+                  }
+                }
+              }
+            }
+            date(formatString: "DD/MM/YYYY")
+          }
+        }
+      }
+    }
+  `);    
+
+  
   return (
     <div className="container">
       <h1 className="title is-2 mt-4 has-text-centered">L'association</h1>
