b66597492953f484b1bd56fb566220e4125e8a3a
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-11-25 01:18:39 +0100

    Nos equipes page (#42)
    * feat : new page organization
    
    * style : scss associated to nos equipes page
    
    * style : New title banner left
    
    * style : h1 title for nos equipes
    
    * feat: New img banner + introduction text with background
    
    * Banner for nos équipes page
    
    * style : selected the correct banner img
    
    * fix : banner positioning and size
    
    * Replace files
    
    * Delete src/images/team-banner.png
    
    * style : New team page banner
    
    * Delete src/images/Bateau_navigation_L.Beauverger-APECS.png
    
    * style : new team banner
    
    * style: team banner source modification
    
    * style : new size for image
    
    * style: new image source for the banner
    
    * style : padding increase

--- src/pages/association/NosEquipes.js
+++ src/pages/association/NosEquipes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import parse from "html-react-parser";
 import "./NosEquipes.scss";
+import BannerTeam from "../../images/team-banner-XL.png";
 
 function replaceWPCssTeam(WPContent) {
   return WPContent.replaceAll("wp-block-columns", "columns")
@@ -63,6 +64,31 @@ export default function NosEquipes() {
 
   return (
     <div>
+      <div className="banner">
+        <img src={BannerTeam} alt="Team-banner" />
+        <div className="banner-content">
+          <h1>Nos Equipes</h1>
+        </div>
+      </div>
+      <p className="pDefault">
+        {" "}
+        Notre association est constituée de nombreux membres aux status et aux
+        fonctions différentes. Chacun à son échelle contribue à la bonne
+        réalisation des projets et des missions de l'APECS. <br /> Le coeur de
+        l'association, ce sont nos salariés. Forts de leurs compétences et de
+        leurs expertises, ils ont en charge la mise en œuvre des projets. Ils
+        travaillent dans nos locaux et sont heureux de vous y acceuillir. Ils
+        sont régulièrement épaulés par nos volontaires en service civique et nos
+        stagiaires qui viennent découvrir le monde associatif, la préservation
+        et l'étude des élasmobranches.
+        <br /> Notre association est gérée par un Conseil d’Administration
+        composé uniquement de bénévoles. Chaque année, lors de l'Assemblée
+        Générale Ordinaire, l'ensemble des adhérents participent à l'élection de
+        ce conseil. Le conseil d'administration 2023/2024 se compose de 10
+        membres aux profils très variés. Un bureau composé d'un président, d'une
+        trésorière et d'un secrétaire a été élu par le conseil après l'assemblée
+        générale 2023. <br /> <br />
+      </p>
       <div className="container">
         <div className="columns is-mobile has-text-centered mb-6">
           <div className="columns">
@@ -71,7 +97,6 @@ export default function NosEquipes() {
         </div>
         <WPcontentTeam content={posts[0].node.content}></WPcontentTeam>
       </div>
-      <div></div>
     </div>
   );
 }
